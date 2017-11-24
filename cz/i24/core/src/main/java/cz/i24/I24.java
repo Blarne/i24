435905f94663cbbdadc8e336f187f8bd5bedbe71
Mira Svoboda <ing.miroslav.svoboda@gmail.com>
2017-11-24 02:22:33 +0100

    Update libraries

---
+++ cz/i24/core/src/main/java/cz/i24/I24.java
@@ -0,0 +1,13 @@
+package cz.i24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I24 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I24.class, args);
+    }
+
+}
